cae6943fa966ea0989fde0c9e8c7598442469556
Shashwat Singh <shashwat.s@recruitriders.com>
2025-09-09 12:22:30 -0400

    env file changed

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,7 +1,7 @@
-
 import { createClient } from '@supabase/supabase-js'
-const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_KEY = process.env.SUPABASE_KEY
 
+// VITE_ variables use karo
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
-
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
